f5f008f06fc8c89522cae3697e4d64164296e2a1
futurist <jamesyang999@gmail.com>
2017-03-24 18:06:46 +0800

    1.0.0 m_drag -> mdrag

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+// rollup.config.js
+
+import buble from 'rollup-plugin-buble'
+import minify from 'rollup-plugin-minify'
+
+
+export default {
+  entry: './lib/mdrag.js',
+  moduleName: 'mdrag',
+  plugins:[
+    buble(),
+    minify({iife: 'dist/mdrag.min.js'})
+  ],
+  targets: [
+    { format: 'es',   dest: 'dist/mdrag.es.js' },
+    { format: 'cjs',  dest: 'dist/mdrag.cjs.js' },
+    { format: 'amd',  dest: 'dist/mdrag.amd.js' },
+    { format: 'iife', dest: 'dist/mdrag.iife.js' },
+  ]
+}
